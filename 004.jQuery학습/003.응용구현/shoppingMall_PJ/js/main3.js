6b909f90ba906f63a084d78a17545fa7a0482ceb
SUE <seungyeonkim2024@u.northwestern.edu>
2022-07-28 14:45:54 +0900

    .

--- 004.jQuery학습/003.응용구현/shoppingMall_PJ/js/main3.js
+++ 004.jQuery학습/003.응용구현/shoppingMall_PJ/js/main3.js
@@ -6,6 +6,8 @@ $(()=>{
     // 호출확인
     console.log("로딩완료!");
 
+
+
     /************************************************ 
         
     [ 페이드 배너 요구사항 ]
@@ -27,9 +29,9 @@ $(()=>{
         // 이벤트 대상: .abtn
         // 이벤트: click() 메서드 사용
         // 양쪽버튼 구분: .ab1(왼쪽버튼) / .ab2(오른쪽버튼)
-        // 변경대상: #slide
+        // 변경대상: #slide li
         // 변경내용: 슬라이드의 top값을 이동하여 애니메이션함
-        let slide = $("#slide");
+        let slide = $("#slide li");  //li까지 선택함!
 
         // 변경에 사용할 제이쿼리 메서드: 
         // animate({CSS속성},시간,이징,함수)
@@ -46,6 +48,17 @@ $(()=>{
         // 애니메이션 이징 변수
         const aniE = "easeInOutCubic";
 
+
+    // fade 기능을 위한 초기화 설정
+    // slide li가 하나만 빼고 모두 불필요!
+    // display:none 처리함! -> hide() 메서드
+    slide.hide().first().show();
+    // 슬라이드li.다숨겨().첫번째().보여()
+
+    // 현재 슬라이드 순번 변수
+    let sno = 0;
+
+        // 버튼 클릭 설정 ////////////
         $(".abtn").click(function(){
 
             // console.log("진입:",prot);
@@ -59,7 +72,7 @@ $(()=>{
             // console.log("통과:",prot);
 
             // 자동넘김 지우기함수 호출!
-            // clearAuto();
+            clearAuto();
 
             // 1. 오른쪽여부
             // is(클래스/아이디명) -> 선택요소해당여부 리턴
@@ -73,19 +86,48 @@ $(()=>{
             if(isR){  //오른쪽버튼  
                 
 
+                
+                // 현재슬라이드 숨기기
+                // faceIn으로 다음순번 보이기
+                // eq(순번) -> 해당순번 선택
+                slide.eq(sno).fadeOut(aniT);
+                 // 슬라이드 순번 1증가
+                 sno++;
+                //  슬라이드 한계값체크 처음으로 변경!
+                if(sno===slide.length) sno=0;
+                // slide.length는 li개수
+                console.log("현재슬번:",sno);
+                // 다음순번 슬라이드 보이기
+                slide.eq(sno).fadeIn(aniT);
+
+              
             } //////////// if ///////////
 
             else{ //윗쪽버튼
+                // faceIn으로 이전순번 보이기
+                // eq(순번) -> 해당순번 선택
+                slide.eq(sno).fadeOut(aniT);
+                 // 슬라이드 순번 1감소
+                 sno--;
+                //  슬라이드 한계값체크 처음으로 변경!
+                if(sno===-1) sno=slide.length-1;
+                // slide.length는 li개수
+                console.log("현재슬번:",sno);
+                // 다음순번 슬라이드 보이기
+                slide.eq(sno).fadeIn(aniT);
 
                
 
             } /////// else //////////
 
             // 3. 등장슬라이드와 같은 순번의 블릿변경하기
-            // 변경내용: 블릿 li에 class="on"을 주고
-            // 나머지 li에는 class="on"을 지운다!
+            // 현재슬라이드번호(sno)와 같은 순번의 블릿 클래스 on
+            indic.eq(sno).addClass("on")
+            // 다른 형제들 블릿 클래스 제거
+            .siblings().removeClass("on");
 
-         
+
+            
 
         }); ////////// click ////////////
 
@@ -103,15 +145,31 @@ $(()=>{
 
         // 타임아웃용 변수
         let autoT;
-
+ 
         // 인터발 최초호출
-        // autoSlide();
+        autoSlide();
 
         // 인터발 호출함수
         function autoSlide(){
 
             autoI = setInterval(()=>{
-               
+         
+                slide.eq(sno).fadeOut(aniT,aniE);
+                // 슬라이드 순번 1증가
+                sno++;
+               //  슬라이드 한계값체크 처음으로 변경!
+               if(sno===slide.length) sno=0;
+               // slide.length는 li개수
+               console.log("현재슬번:",sno);
+               // 다음순번 슬라이드 보이기
+               slide.eq(sno).fadeIn(aniT,aniE);
+
+    
+                // 3. 등장슬라이드와 같은 순번의 블릿변경하기
+                // 현재슬라이드번호(sno)와 같은 순번의 블릿 클래스 on
+                indic.eq(sno).addClass("on")
+                // 다른 형제들 블릿 클래스 제거
+                .siblings().removeClass("on");
 
             },3000); ////// 인터발함수 ///////
         } //////////////// autoSlide함수 //////////////
