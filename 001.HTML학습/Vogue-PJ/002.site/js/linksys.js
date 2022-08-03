584ee9c073dbaa47e4b86026eeda7237f287cad9
SUE <seungyeonkim2024@u.northwestern.edu>
2022-08-03 17:22:30 +0900

    .

--- 001.HTML학습/Vogue-PJ/002.site/js/linksys.js
+++ 001.HTML학습/Vogue-PJ/002.site/js/linksys.js
@@ -4,10 +4,10 @@ $(()=>{ ////////// jQB ///////////////////////
 
     // 로고 클릭시 첫페이지 이동하기/////////////
     $(".logo a").click(()=>location.href='index.html');
-    
+
     // GNB 링크 셋팅 /////////////////////
     // 대상: .gnb a
-    $('.gnb a').click(function(e){
+    $('.gnb a,.mognb a').click(function(e){
 
         // a기본이동 막기
         e.preventDefault();
